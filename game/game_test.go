b9d38a6ad8bbc86287f429277cb455232ec56935
Paweł Tymura <paweltymura@op.pl>
2015-03-06 09:19:05 +0100

    Added assert package to tests, little cleanup

--- game/game_test.go
+++ game/game_test.go
@@ -1,6 +1,10 @@
 package game
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestGameShouldStart(t *testing.T) {
 	Game(0)
@@ -8,53 +12,46 @@ func TestGameShouldStart(t *testing.T) {
 
 func TestGenerateDirectionLeft(t *testing.T) {
 	var value, _ = generateDirection(0)
-	if value != "<" {
-		t.Error("Expect \"<\", but was " + value)
-	}
+
+	assert.Equal(t, "<", value, "they should be equal")
 }
 
 func TestGenerateDirectionRight(t *testing.T) {
 	var value, _ = generateDirection(1)
-	if value != ">" {
-		t.Error("Expect \">\", but was " + value)
-	}
+
+	assert.Equal(t, ">", value, "they should be equal")
 }
 
 func TestGenerateDirectionUp(t *testing.T) {
 	var value, _ = generateDirection(2)
-	if value != "^" {
-		t.Error("Expect \"^\", but was " + value)
-	}
+
+	assert.Equal(t, "^", value, "they should be equal")
 }
 
 func TestGenerateDirectionDown(t *testing.T) {
 	var value, _ = generateDirection(3)
-	if value != "v" {
-		t.Error("Expect \"v\", but was " + value)
-	}
+
+	assert.Equal(t, "v", value, "they should be equal")
 }
 
 func TestGenerateDirectionShoudError(t *testing.T) {
 	var _, err = generateDirection(4)
-	if err == nil {
-		t.Error("Expect err, but error was nil")
-	}
+
+	assert.Nil(t, err)
 }
 
 func Test_game_ShouldReturnTrueIfUserWin(t *testing.T) {
 	genDirectionsStub := func(quant int) string { return "<<<" }
 	getRspGameStub := func() string { return "<<<" }
 	countingToXStub := func(quant int) {}
-	if game(3, genDirectionsStub, getRspGameStub, countingToXStub) == false {
-		t.Error("Expect true but was false")
-	}
+
+	assert.False(t, game(3, genDirectionsStub, getRspGameStub, countingToXStub))
 }
 
 func Test_game_ShouldReturnFalseIfUserLose(t *testing.T) {
 	genDirectionsStub := func(quant int) string { return "<<<" }
 	getRspGameStub := func() string { return ">>>" }
 	countingToXStub := func(quant int) {}
-	if game(3, genDirectionsStub, getRspGameStub, countingToXStub) {
-		t.Error("Expect false but was true")
-	}
+
+	assert.True(t, game(3, genDirectionsStub, getRspGameStub, countingToXStub))
 }
